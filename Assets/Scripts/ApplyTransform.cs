e20b977aba66797eca02f6cd40c831deafdbabc0
Pablo Banzo <pridapablo@gmail.com>
2023-11-06 13:29:53 -0600

    fix script

--- Assets/Scripts/ApplyTransform.cs
+++ Assets/Scripts/ApplyTransform.cs
@@ -1,54 +1,74 @@
+/* Use matrices to modify the vertices of a mesh 
+
+Note: the child has the mesh, not the parent object
+
+Pablo Banzo Prida
+2023-11-02
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplyTransforms : MonoBehaviour
+public class ApplyTransform : MonoBehaviour
 {
-[SerializeField] Vector3 displacement;
-[SerializeField] float angle;
-[SerializeField] AXIS rotationAxis;
+    [SerializeField] Vector3 displacement;
+    [SerializeField] float angle;
+    [SerializeField] AXIS rotationAxis;
 
-Mesh mesh;
-Vector3[] baseVertices;
-Vector3[] newVertices;
-void Start(){
-mesh = GetComponentInChildren<MeshFilter>().mesh;
-baseVertices = mesh.vertices;
+    // Mesh class to store the mesh of the object
+    Mesh mesh;
+    Vector3[] baseVertices; // Original vertices
+    Vector3[] newVertices;
 
-newVertices = new Vector3[baseVertices.Length];
-for (int i = 0; i < baseVertices.Length; i++){
-newVertices[i] = baseVertices[i];
-}
+    void Start()
+    {
+        // get the mesh of the object's child (of type MeshFilter)
+        mesh = GetComponentInChildren<MeshFilter>().mesh;
+        // get the vertices of the mesh
+        baseVertices = mesh.vertices;
+
+        // Create a copy of the original vertices
+        newVertices = new Vector3[baseVertices.Length];
+        for (int i = 0; i < baseVertices.Length; i++)
+        {
+            newVertices[i] = baseVertices[i];
+        }
 
 
-}
+    }
 
-// Update is called once per frame
-void Update(){
-DoTransform();
-}
+    // Update is called once per frame
+    void Update()
+    {
+        DoTransform();
 
-void DoTransform(){
-Matrix4x4 move = HW_Transforms.TranslationMat(displacement.x*Time.deltaTime,
-displacement.y*Time.deltaTime,
-displacement.z*Time.deltaTime);
+    }
 
-Matrix4x4 rotate = HW_Transforms.RotateMat(angle * Time.deltaTime,
-rotationAxis);
+    void DoTransform()
+    {
+        Matrix4x4 move = HW_Transforms.TranslationMat(
+            displacement.x * Time.deltaTime, // we add Time.deltaTime to make the movement frame rate independent
+            displacement.y * Time.deltaTime,
+            displacement.z * Time.deltaTime
+        );
 
-Matrix4x4 posOrigin = HW_Transforms.TranslationMat(-displacement.x,
--displacement.y,
--displacement.z);
+        Matrix4x4 composite = move;
 
-Matrix4x4 posObject = HW_Transforms.TranslationMat(displacement.x,
-displacement.y,
-displacement.z);
-Matrix4x4 composite = posObject*rotate*posOrigin;
 
-for (int i = 0; i<newVertices.Length; i++){
-Vector4 temp = new Vector4(newVertices[i].x,newVertices[i].y,newVertices[i].z,1);
-newVertices[i] = composite * temp;
-}
-mesh.vertices = newVertices;
-}
+        for (int i = 0; i < newVertices.Length; i++)
+        {
+
+            Vector4 temp = new Vector4(
+                newVertices[i].x,
+                newVertices[i].y,
+                newVertices[i].z, 1
+                );
+            newVertices[i] = composite * temp;
+        }
+
+        // Replace the vertices of the mesh with the new ones
+        mesh.vertices = newVertices;
+
+
+    }
 }
